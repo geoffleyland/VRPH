864497c23880947177c9328488b8b406669253a4
Geoff Leyland <geoff.leyland@incremental.co.nz>
2014-10-13 16:01:47 +1300

    make it easy to turn on all the debugging in VRPDebug.h

--- inc/VRPDebug.h
+++ inc/VRPDebug.h
@@ -13,72 +13,74 @@
 #ifndef _VRP_DEBUG_H
 #define _VRP_DEBUG_H
 
-#define FIXED_DEBUG             0
-#define    SEARCH_DEBUG         0
-#define VRPH_TABU_DEBUG         0
-#define BLOAT_DEBUG             0
-#define WAREHOUSE_DEBUG         0
+#define DEBUG_ALL               0
 #define VERIFY_ALL              0
 
-#define CW_DEBUG                0
+#define FIXED_DEBUG             0 + DEBUG_ALL
+#define    SEARCH_DEBUG         0 + DEBUG_ALL
+#define VRPH_TABU_DEBUG         0 + DEBUG_ALL
+#define BLOAT_DEBUG             0 + DEBUG_ALL
+#define WAREHOUSE_DEBUG         0 + DEBUG_ALL
+
+#define CW_DEBUG                0 + DEBUG_ALL
 #define CW_VERIFY               0 + VERIFY_ALL
 
-#define CLEAN_DEBUG             0
+#define CLEAN_DEBUG             0 + DEBUG_ALL
 
-#define Q_DEBUG                 0
+#define Q_DEBUG                 0 + DEBUG_ALL
 #define Q_VERIFY                0 + VERIFY_ALL
 
-#define OP_VERIFY               0
+#define OP_VERIFY               0 + DEBUG_ALL
 
-#define STRING_DEBUG            0
+#define STRING_DEBUG            0 + DEBUG_ALL
 #define STRING_VERIFY           0 + VERIFY_ALL
 
 #define OPM_VERIFY              0 + VERIFY_ALL
-#define OPM_DEBUG               0    
+#define OPM_DEBUG               0 + DEBUG_ALL
 
 #define OR_VERIFY               0 + VERIFY_ALL
-#define OR_DEBUG                0
+#define OR_DEBUG                0 + DEBUG_ALL
 
 #define POSTSERT_VERIFY         0 + VERIFY_ALL
-#define POSTSERT_DEBUG          0
+#define POSTSERT_DEBUG          0 + DEBUG_ALL
 
 #define PRESERT_VERIFY          0 + VERIFY_ALL
-#define PRESERT_DEBUG           0
+#define PRESERT_DEBUG           0 + DEBUG_ALL
 
-#define FLIP_DEBUG              0
+#define FLIP_DEBUG              0 + DEBUG_ALL
 #define FLIP_VERIFY             0 + VERIFY_ALL
 
-#define SWAP_ENDS_DEBUG         0
+#define SWAP_ENDS_DEBUG         0 + DEBUG_ALL
 #define SWAP_ENDS_VERIFY        0 + VERIFY_ALL
 
-#define SWAP_DEBUG              0
+#define SWAP_DEBUG              0 + DEBUG_ALL
 #define SWAP_VERIFY             0 + VERIFY_ALL
 
-#define REVERSE_DEBUG           0    
+#define REVERSE_DEBUG           0 + DEBUG_ALL
 #define REVERSE_VERIFY          0 + VERIFY_ALL
 
 #define SWAP_VERIFY             0 + VERIFY_ALL
-#define SWAP_DEBUG              0 
+#define SWAP_DEBUG              0 + DEBUG_ALL
 
-#define CONCATENATE_DEBUG       0
+#define CONCATENATE_DEBUG       0 + DEBUG_ALL
 #define CONCATENATE_VERIFY      0 + VERIFY_ALL
 
 
-#define TPM_DEBUG               0
+#define TPM_DEBUG               0 + DEBUG_ALL
 #define TPM_VERIFY              0 + VERIFY_ALL
 
-#define TWO_OPT_DEBUG           0
+#define TWO_OPT_DEBUG           0 + DEBUG_ALL
 #define TWO_OPT_VERIFY          0 + VERIFY_ALL
 
-#define THREE_OPT_DEBUG         0
+#define THREE_OPT_DEBUG         0 + DEBUG_ALL
 #define THREE_OPT_VERIFY        0 + VERIFY_ALL
 
-#define CROSS_EXCHANGE_DEBUG    0
+#define CROSS_EXCHANGE_DEBUG    0 + DEBUG_ALL
 #define CROSS_EXCHANGE_VERIFY   0 + VERIFY_ALL
 
 
-#define NEIGHBOR_DEBUG          0
-#define TSPLIB_DEBUG            0
+#define NEIGHBOR_DEBUG          0 + DEBUG_ALL
+#define TSPLIB_DEBUG            0 + DEBUG_ALL
 
 
 void report_error(const char* format, ...);
